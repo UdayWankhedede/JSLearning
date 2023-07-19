bc6f1feaaf282fa3b21167450a0aadc755c70178
Uday Wankhede <103430876+UdayWankhedede@users.noreply.github.com>
2023-07-19 22:08:11 +0530

    basic programming
    basic programming

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+var myName; //Variable Declaration
+
+myFirstName = "Uday"; //Variable Assignment or Initialization
+
+console.log(myFirstName);
+
+var city = "Pune"; //variable declaration annd initialization
+console.log(city);
+
+var pin;
+console.log(pin); // undifined is like garbage
---
+++ hello.js
@@ -0,0 +1,2 @@
+console.log("Hello World");//this is use to write output on console
+console.log("Jay Ganesh"); 

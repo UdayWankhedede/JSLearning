8d296ba2ede205bc6b69ef218c1ef0b581bf3299
Uday Wankhede <103430876+UdayWankhedede@users.noreply.github.com>
2023-08-08 22:45:23 +0530

    Both assignment are done
    Both assignment are done

---
+++ Assignment/10_objectAssignB.js
@@ -0,0 +1,46 @@
+let bankSBI ={
+    bankName : "SBI",
+    branch : "Arvi",
+    branchCode : 101,
+    state : "Maharashtra"
+}
+
+let bankLocation = {
+    street : "wardha road",
+    city : "Arvi",
+    pincode : 442201
+}
+
+
+console.log("================================================Using Object.assign================================================");
+
+let sbiClone = Object.assign({},bankSBI);
+console.log(`Bank Name is ${sbiClone.bankName} located at ${sbiClone.branch} state ${sbiClone.state} having branch code ${sbiClone.branchCode}`);
+let locationClone = Object.assign({},bankLocation);
+console.log(`Bank SBI is located near ${locationClone.street} at ${locationClone.city} taluka having pin code ${locationClone.pincode}`);
+
+console.log("================================================Using Spread================================================");
+let cloneSBIBank = bankSBI;
+console.log(`Bank Name is ${sbiClone.bankName} located at ${sbiClone.branch} state ${sbiClone.state} having branch code ${sbiClone.branchCode}`);
+//console.log(cloneSBIBank);
+let cloneLocation = bankLocation;
+//console.log(cloneLocation);
+console.log(`Bank SBI is located near ${locationClone.street} at ${locationClone.city} taluka having pin code ${locationClone.pincode}`);
+
+
+let rateOfInterest ={
+    homeLoadInterest : 5,
+    personalLoadInterest : 15,
+    dueLoadInterest : 5
+}
+let sbiDetails = Object.assign({},bankSBI,bankLocation,rateOfInterest)
+console.log(`Bank Name is ${sbiClone.bankName} located at ${sbiClone.branch} state ${sbiClone.state} having branch code ${sbiClone.branchCode} Bank SBI is located near ${locationClone.street} at ${locationClone.city} taluka having pin code ${locationClone.pincode}\nhaving rate of interest \nHome Loan :${sbiDetails.homeLoadInterest}\nDue Loan :${sbiDetails.dueLoadInterest}\nPersonal Loan :${sbiDetails.personalLoadInterest}`);
+
+
+console.log("================================================Using For of Loop================================================");
+
+
+for(const value in sbiDetails)
+{
+    console.log(`${value} : ${sbiDetails[value]}`);
+}
---
+++ Assignment/10_objectCloneAssigA.js
@@ -0,0 +1,34 @@
+let personalDetails = {
+  pname: "Uday",
+  place: "Arvi",
+  gender: "Male",
+};
+let collegeDetails = {
+  collegeName: "GH Raisoni",
+  city: "Amravati",
+  branch: "Civil Engineering",
+};
+
+let mergedObjects = Object.assign({}, personalDetails, collegeDetails);
+console.log(
+  `My Name is : ${mergedObjects.pname} i am from ${mergedObjects.place} and i am ${mergedObjects.gender}, My College Name is ${mergedObjects.collegeName} present at ${mergedObjects.city} and passout from ${mergedObjects.branch} department`
+);
+
+const friendArray = ["Uday", "Piyush", "Aditi", "Adesh", "Ashu"];
+Object.freeze(friendArray);
+
+for (let value of friendArray) {
+  console.log(value);
+}
+
+
+let string = "Codemind Technology"
+let newstring = string.substring(9)
+let newstring1 = string.substring(0,9)// for final string concatination
+let reverseString=""
+for (let index = newstring.length-1; index >= 0 ; index--) {
+    const element = newstring.charAt(index);
+    reverseString =reverseString.concat(element) 
+}
+let fstring = newstring1.concat(reverseString) 
+console.log(fstring);
